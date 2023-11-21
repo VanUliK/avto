a8a4a70fe9dabe488fb77ec85b88c58216d48e96
VanUliK <vanulik@work.com>
2023-11-21 14:34:35 +0200

    reload

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Get the DOM elements for the image carousel
 const wrapper = document.querySelector(".wrapper"),
   carousel = document.querySelector(".carousel"),
-  images = document.querySelectorAll("img"),
+  images = document.querySelectorAll(".carousel img"),
   buttons = document.querySelectorAll(".button");
 
 let imageIndex = 1,
